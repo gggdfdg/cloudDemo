24dddb1f19ef03f97ce447feebe9c44b5c99860b
kj-9659 <eboy1992@126.com>
2018-09-14 15:04:51 +0800

    上传配置文件

--- cloudDemo-db/src/main/java/com/cloudDemo/db/domain/Ad.java
+++ cloudDemo-db/src/main/java/com/cloudDemo/db/domain/Ad.java
@@ -1,4 +1,12 @@
 package com.cloudDemo.db.domain;
 
+import lombok.Data;
+
+@Data
 public class Ad {
+   private Integer id;
+   private String name;
+   private String link;
+   private byte position;
+   private String content;
 }
